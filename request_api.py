3a3ab79ff5851f11bd60f6610ceaf5cccbfbed2d
Seski Ramadhan <seski.ramadhan@gmail.com>
2020-08-05 11:28:12 +0700

    interactive request api

--- request_api.py
+++ request_api.py
@@ -1,6 +1,8 @@
 import requests
 
-resp = requests.get('http://seskiramadhan.pythonanywhere.com/api?id=9')
+input=input('Inputkan ID untuk request API\n')
+
+resp = requests.get('http://seskiramadhan.pythonanywhere.com/api?id='+input)
 data = resp.json()
 data = data[0]
 data = data["katakata"]
